f5d6eb80695baa888cedfc41835f61b81e94beba
Wallans <48529533+JohnWallans@users.noreply.github.com>
2022-11-25 17:49:01 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -20,7 +20,9 @@
             <h1 class="display-4">Inscription</h1>
             <p class="lead">Merci de vous inscrire</p>
         </div>
+
         <form method="post" action="" id="form" novalidate>
+
             <div class="form-row">
                 <div class="form-control-group col-md-3">
                     <label for="email">Adresse électronique :</label>
@@ -30,18 +32,55 @@
                         L'adresse email doit être de 2 à 30 caractères.
                     </div>
                 </div>
-            </div>
 
-            <div class="form-row">
                 <div class="form-control-group col-md-3">
                     <label for="mdp">Mot de passe :</label>
                     <input pattern="[A-Za-zéèà. ]{8,30}" class="form-control" name="mdp" id="mdp"
                         placeholder="Ex : !(F4r58?7g" required>
                     <div class="invalid-feedback">
-                        Le mot de passe doit être entre
+                        Le mot de passe doit être de 8 à 30 caractères.
                     </div>
                 </div>
             </div>
+
+            <br>
+
+            <div class="form-row">
+                <div class="form-control-group col-md-3">
+                    <label for="name">Nom :</label>
+                    <input pattern="[A-Za-zéèà. ]{2,20}" class="form-control" name="name" id="name"
+                        placeholder="Ex : DUJARDIN" required>
+                    <div class="invalid-feedback">
+                        Le nom doit être de 2 à 20 caractères.
+                    </div>
+                </div>
+
+                <div class="form-control-group col-md-3">
+                    <label for="fisrtname">Prénom :</label>
+                    <input pattern="[A-Za-zéèà. ]{2,20}" class="form-control" name="fisrtname" id="fisrtname"
+                        placeholder="Ex : Jean" required>
+                    <div class="invalid-feedback">
+                        Le prénom doit être de 2 à 20 caractères.
+                    </div>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="form-row">
+                <div class="form-control-group col-md-3">
+                    <label for="typeAccount">Type de compte :</label>
+                    <select class="form-control col-md-6" name="typeAccount" id="typeAccount">
+                        <option value="client">Client</option>
+                        <option value="photographe">Photographe</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        Le nom doit être de 2 à 20 caractères.
+                    </div>
+                </div>
+            </div>
+
+
             <br>
             <div class="form-row">
                 <button class="btn btn-primary" value="submit" name="valider">Inscription</button>
