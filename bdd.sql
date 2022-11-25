2071b483f066783efd1a618b43a0d6c802658b9f
Wallans <48529533+JohnWallans@users.noreply.github.com>
2022-11-25 17:49:21 +0100

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -2,7 +2,7 @@ CREATE DATABASE photoforyou_bdd;
 
 USE photoforyou_bdd;
 
-CREATE TABLE UTILISATEUR(IdUser int PRIMARY KEY AUTO_INCREMENT, EmailUser varchar(30), MdpUser varchar(64));
+CREATE TABLE UTILISATEUR(IdUser int PRIMARY KEY AUTO_INCREMENT, EmailUser varchar(30), MdpUser varchar(64), NomUser varchar(30), PrenomUser varchar(30));
 
 CREATE TABLE PHOTOGRAPHE(IdPhotographe int, Test int, PRIMARY KEY (IdPhotographe), FOREIGN KEY (IdPhotographe) REFERENCES UTILISATEUR(IdUser));
 
@@ -11,9 +11,9 @@ CREATE TABLE CLIENT(IdClient int, Crédit int, PRIMARY KEY (IdClient), FOREIGN K
 CREATE TABLE ADMINISTRATEUR(IdAdministrateur int, Grade varchar(30), PRIMARY KEY (IdAdministrateur), FOREIGN KEY (IdAdministrateur) REFERENCES UTILISATEUR(IdUser));
 
 INSERT INTO UTILISATEUR(EmailUser, MdpUser) VALUES 
-("test1@gmail.com", "1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014"),
-("test2@gmail.com", "60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752"),
-("test3@gmail.com", "fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13");
+("test1@gmail.com", "1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014", "DUJARDIN", "Jean"),
+("test2@gmail.com", "60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752", "CLAVIER", "Christian"),
+("test3@gmail.com", "fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13", "CANET", "Guillaume");
 
 
 INSERT INTO PHOTOGRAPHE(IdPhotographe, Test) VALUES 
